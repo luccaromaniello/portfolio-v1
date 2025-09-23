cfdc4d77b125263dbb2f9b99c323ceccdb0a1f8a
Lucca Romaniello <37159767+luccaromaniello@users.noreply.github.com>
2025-09-23 13:29:11 -0300

    feat: updates tailwind usage

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,33 +1,6 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-        primary: {
-          main: "#AE1E5F",
-          background: "#FFEAF2",
-        },
-        content: {
-          primary: "#0A0A0A",
-          secondary: "#6F6F6F",
-          tertiary: "#B3B5BB",
-          highlight: "#FDFD96",
-        },
-        feedback: {
-          success: "#176D38",
-        },
-        neutral: {
-          white: "#FFFFFF",
-        },
-      },
-    },
-  },
+  content: ["./src/**/*.{astro,js,ts,jsx,tsx,mdx}"],
   plugins: [],
 } satisfies Config;
