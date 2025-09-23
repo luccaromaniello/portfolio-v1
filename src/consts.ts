3c88ebcbc79979b2fd995af9063403b9459871ae
Lucca Romaniello <37159767+luccaromaniello@users.noreply.github.com>
2025-09-23 12:04:07 -0300

    feat: add base structure for projects

--- src/consts.ts
+++ src/consts.ts
@@ -1,10 +1,17 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Lucca Romaniello';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "Lucca Romaniello";
+export const SITE_DESCRIPTION = "Welcome to my website!";
 
 export const GITHUB_URL = "https://github.com/luccaromaniello/";
 export const LINKEDIN_URL = "https://www.linkedin.com/in/luccaromaniello/";
 export const MAIL_URL = "mailto:contact@luccaromaniello.com";
 export const WEBSITE_URL = "https://www.luccaromaniello.com";
+
+// projects
+export const PROJECT_ODIN_LIVE_URL =
+  "https://github.com/luccaromaniello/theodinproject";
+export const PROJECT_ODIN_REPO_URL =
+  "https://github.com/luccaromaniello/theodinproject";
+export const PROJECT_DESIGN_LIVE_URL = "https://www.v0.luccaromaniello.com";
+
+// styles
+export const TOP_NAV_HEIGHT = "56px";
