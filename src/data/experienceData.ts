3cf2d036c29c7fb6c74f8e028cc0363c2e9153cb
Lucca Romaniello <37159767+luccaromaniello@users.noreply.github.com>
2025-09-30 00:11:57 -0300

    refactor: experience description

--- src/data/experienceData.ts
+++ src/data/experienceData.ts
@@ -6,7 +6,7 @@ export const workExperience = [
     location: "Remote",
     sector: "Higher education",
     description:
-      "Designed and delivered impactful digital experiences across industries including beverages (E-commerce/SaaS), telecom, healthcare, and education. Led key projects that improved usability, efficiency, and user satisfaction, such as redesigning AB InBev’s developer portal, optimizing fluke’s SIM card flow, enhancing Conexa Saúde’s doctor tools, and building a certificate platform for Ânima Educação serving over 350,000 students.",
+      "I’ve designed and delivered digital experiences across industries like beverages (e-commerce/SaaS), telecom, healthcare, and education. I led projects that improved usability and efficiency, including the redesign of AB InBev’s developer portal, optimization of fluke’s SIM card flow, enhancements to Conexa Saúde’s doctor tools, and the development of a certificate platform for Ânima Educação, serving over 350,000 students.",
     technologies: [
       "UI/UX design",
       "Design systems",
@@ -33,8 +33,8 @@ export const workExperience = [
     location: "Belo Horizonte, Brazil",
     sector: "Urban mobility",
     description:
-      "Localiza&Co offers complete mobility solutions for individuals and businesses, including car rentals, car subscriptions, fleet outsourcing, and sales of vehicles, operating across 7 countries in Latin America through franchises. There, I was a mobile and backend developer for an app migration from hybrid to native used by their carport operators. I also worked as frontend developer on internal software from dti used for visual management and merit money.",
-    technologies: ["Kotlin", "Vue.js", "Flutter", "ASP.NET"],
+      "Localiza&Co offers mobility solutions across seven Latin American countries, including car rentals, subscriptions, fleet outsourcing, and vehicle sales. While there, I worked as a mobile and backend developer on an app migration from hybrid to native, used by carport operators. I also contributed as a frontend developer to internal tools built by dti, supporting visual management and merit-based recognition.",
+    technologies: ["Vue.js", "Kotlin", "Flutter", "ASP.NET"],
     companies: [
       {
         name: "dti digital + Localiza&Co",
