6b0d2c3b3cf76e06e450e4c0b64b7b8f7a337346
Lucca Romaniello <37159767+luccaromaniello@users.noreply.github.com>
2025-09-30 22:01:01 -0300

    refactor: experiences to be a component

--- src/data/educationData.ts
+++ src/data/educationData.ts
@@ -1,12 +1,31 @@
 export const educationHistory = [
   {
-    companies: [],
-    description:
-      "Self-taught: always learning from every book, article, co-worker, and life experience",
-    highlight: false,
-    startDate: "Always",
-    location: "Anywhere",
+    companies: [
+      {
+        name: "The Odin Project",
+        website: "https://www.theodinproject.com/",
+      },
+    ],
+    title: "The Odin Project",
+    startDate: "2025",
+    location: "Remote",
     current: true,
+    technologies: ["Vue.js", "Kotlin", "Flutter", "ASP.NET"],
+  },
+  {
+    companies: [
+      {
+        name: "Y Combinator ",
+        website: "https://www.ycombinator.com/",
+      },
+    ],
+    title: "Startup School",
+    highlight: false,
+    startDate: "2025",
+    endDate: "2025",
+    location: "Remote",
+    current: false,
+    technologies: ["Vue.js", "Kotlin", "Flutter", "ASP.NET"],
   },
   {
     companies: [
@@ -15,12 +34,12 @@ export const educationHistory = [
         website: "https://www.linkedin.com/school/pucminas/",
       },
     ],
-    description: "Full Stack Development, Postgraduate degree (Lato sensu)",
-    highlight: true,
+    title: "Full Stack Development, Postgraduate degree (Lato sensu)",
     startDate: "2024",
     endDate: "2025",
     location: "Remote",
     current: false,
+    technologies: ["Vue.js", "Kotlin", "Flutter", "ASP.NET"],
   },
   {
     companies: [
@@ -29,11 +48,11 @@ export const educationHistory = [
         website: "https://www.linkedin.com/school/pucminas/",
       },
     ],
-    description: "Software Engineering, Bachelor’s degree",
-    highlight: true,
+    title: "Software Engineering, Bachelor’s degree",
     startDate: "2018",
     endDate: "2022",
     location: "Belo Horizonte, Brazil",
     current: false,
+    technologies: ["Vue.js", "Kotlin", "Flutter", "ASP.NET"],
   },
 ];
--- src/data/experienceData.ts
+++ src/data/experienceData.ts
@@ -1,10 +1,9 @@
 export const workExperience = [
   {
-    role: "Product Designer",
+    title: "Product Designer",
     startDate: "Jul 20",
     current: true,
     location: "Remote",
-    sector: "Higher education",
     description:
       "I’ve designed and delivered digital experiences across industries like beverages (e-commerce/SaaS), telecom, healthcare, and education. I led projects that improved usability and efficiency, including the redesign of AB InBev’s developer portal, optimization of fluke’s SIM card flow, enhancements to Conexa Saúde’s doctor tools, and the development of a certificate platform for Ânima Educação, serving over 350,000 students.",
     technologies: [
@@ -26,12 +25,11 @@ export const workExperience = [
     ],
   },
   {
-    role: "Full Stack Developer",
+    title: "Full Stack Developer",
     startDate: "Apr 19",
     endDate: "Jul 20",
     current: false,
     location: "Belo Horizonte, Brazil",
-    sector: "Urban mobility",
     description:
       "Localiza&Co offers mobility solutions across seven Latin American countries, including car rentals, subscriptions, fleet outsourcing, and vehicle sales. While there, I worked as a mobile and backend developer on an app migration from hybrid to native, used by carport operators. I also contributed as a frontend developer to internal tools built by dti, supporting visual management and merit-based recognition.",
     technologies: ["Vue.js", "Kotlin", "Flutter", "ASP.NET"],
