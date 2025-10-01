ed4dd3a16a092e6df888752cdbe03609775fed87
Lucca Romaniello <37159767+luccaromaniello@users.noreply.github.com>
2025-10-01 17:37:54 -0300

    feat: updates footer links to be components

--- src/consts.ts
+++ src/consts.ts
@@ -3,7 +3,7 @@ export const SITE_DESCRIPTION = "Welcome to my website!";
 
 export const GITHUB_URL = "https://github.com/luccaromaniello/";
 export const LINKEDIN_URL = "https://www.linkedin.com/in/luccaromaniello/";
-export const MAIL_URL = "mailto:contact@luccaromaniello.com";
+export const MAIL_URL = "contact@luccaromaniello.com";
 export const WEBSITE_URL = "https://www.luccaromaniello.com";
 
 // projects
@@ -11,7 +11,4 @@ export const PROJECT_ODIN_LIVE_URL =
   "https://github.com/luccaromaniello/theodinproject";
 export const PROJECT_ODIN_REPO_URL =
   "https://github.com/luccaromaniello/theodinproject";
-export const PROJECT_DESIGN_LIVE_URL = "https://www.v0.luccaromaniello.com";
-
-// styles
-export const TOP_NAV_HEIGHT = "56px";
+export const PROJECT_PORTFOLIO_V0_URL = "https://www.v0.luccaromaniello.com";
