fb7e84c249103167a41956bca39e79adc80640e6
Lucca Romaniello <37159767+luccaromaniello@users.noreply.github.com>
2025-10-02 20:06:01 -0300

    feat: adds links to icons

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,11 @@
+import GithubIcon from "@assets/icons/github.svg";
+import LinkedinIcon from "@assets/icons/linkedin.svg";
+import type { Link } from "@typeDefs/link";
+
 export const SITE_TITLE = "Lucca Romaniello";
 export const SITE_DESCRIPTION = "Welcome to my website!";
 
-export const GITHUB_URL = "https://github.com/luccaromaniello/";
+export const GITHUB_URL = "https://www.github.com/luccaromaniello/";
 export const LINKEDIN_URL = "https://www.linkedin.com/in/luccaromaniello/";
 export const MAIL_URL = "contact@luccaromaniello.com";
 export const WEBSITE_URL = "https://www.luccaromaniello.com";
@@ -12,3 +16,22 @@ export const PROJECT_ODIN_LIVE_URL =
 export const PROJECT_ODIN_REPO_URL =
   "https://github.com/luccaromaniello/theodinproject";
 export const PROJECT_PORTFOLIO_V0_URL = "https://www.v0.luccaromaniello.com";
+
+export const SOCIAL_ICONS = {
+  github: {
+    alt: "GitHub icon",
+    icon: GithubIcon,
+    link: {
+      label: "GitHub",
+      url: GITHUB_URL,
+    } as Link,
+  },
+  linkedin: {
+    alt: "LinkedIn icon",
+    icon: LinkedinIcon,
+    link: {
+      label: "LinkedIn",
+      url: LINKEDIN_URL,
+    } as Link,
+  },
+};
