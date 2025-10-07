887e13b9ce612f9e3acda6229a2eeede72a3f7da
Lucca Romaniello <37159767+luccaromaniello@users.noreply.github.com>
2025-10-06 22:21:22 -0300

    feat: adds base top route

--- src/consts.ts
+++ src/consts.ts
@@ -3,7 +3,8 @@ import LinkedinIcon from "@assets/icons/linkedin.svg";
 import type { Link } from "@typeDefs/link";
 
 export const SITE_TITLE = "Lucca Romaniello";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_DESCRIPTION =
+  "Full Stack Engineer with 6+ years experience and a design background, creating scalable, user-friendly products for startups and global companies.";
 
 export const GITHUB_URL = "https://www.github.com/luccaromaniello/";
 export const LINKEDIN_URL = "https://www.linkedin.com/in/luccaromaniello/";
@@ -11,10 +12,13 @@ export const MAIL_URL = "contact@luccaromaniello.com";
 export const WEBSITE_URL = "https://www.luccaromaniello.com";
 
 // projects
-export const PROJECT_ODIN_LIVE_URL =
-  "https://github.com/luccaromaniello/theodinproject";
+export const PROJECT_ODIN_SITE_TITLE = "Lucca Romaniello — The Odin Project";
+export const PROJECT_ODIN_SITE_DESCRIPTION =
+  "Full Stack Engineer Lucca Romaniello presents a collection of 27 full-stack web development challenges completed during The Odin Project curriculum, showcasing practical skills and coding solutions.";
+export const PROJECT_ODIN_LIVE_URL = "/theodinproject/";
 export const PROJECT_ODIN_REPO_URL =
   "https://github.com/luccaromaniello/theodinproject";
+
 export const PROJECT_PORTFOLIO_V0_URL = "https://www.v0.luccaromaniello.com";
 
 export const SOCIAL_ICONS = {
@@ -35,3 +39,7 @@ export const SOCIAL_ICONS = {
     } as Link,
   },
 };
+
+// styles
+export const baseLayout =
+  "px-8 md:px-20 lg:px-28 xl:px-36 grid grid-cols-1 max-w-[1632px] mx-auto";
