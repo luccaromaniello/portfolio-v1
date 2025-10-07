24da09ffb0221f781ad3c57d7293225709115f75
Lucca Romaniello <37159767+luccaromaniello@users.noreply.github.com>
2025-10-06 22:50:38 -0300

    feat: add base for displaying odin projects

---
+++ src/data/odinProjects.ts
@@ -0,0 +1,17 @@
+import topImage from "@assets/placeholder-top.png";
+import { SECTIONS } from "@typeDefs/odinProject";
+
+export const odinProjects = [
+  {
+    title: "Sign-up Form",
+    type: SECTIONS.INTERMEDIATE_HTML_CSS,
+    description:
+      "A collection of 27 challenges created during The Odin Project, a full‑stack web‑development curriculum.",
+    website: "https://luccaromaniello.com/theodinproject/sign-up-form/",
+    repository: "https://github.com/luccaromaniello/top-sign-up-form/",
+    image: {
+      src: topImage,
+      alt: "An overview of The Odin Project's website.",
+    },
+  },
+];
