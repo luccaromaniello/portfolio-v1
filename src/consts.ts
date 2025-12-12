d5edf925a2a24bfb291fff3508a826f554b9f0b7
Lucca Romaniello <37159767+luccaromaniello@users.noreply.github.com>
2025-12-12 20:33:46 -0300

    refactor: base layout padding

--- src/consts.ts
+++ src/consts.ts
@@ -42,4 +42,4 @@ export const SOCIAL_ICONS = {
 
 // styles
 export const baseLayout =
-  "px-8 md:px-20 lg:px-16 xl:px-80 grid grid-cols-1 gap-16 md:gap-4 max-w-[1440px] mx-auto my-16";
+  "px-8 md:px-20 lg:px-16 xl:px-72 grid grid-cols-1 gap-16 md:gap-4 max-w-[1440px] mx-auto my-16";
