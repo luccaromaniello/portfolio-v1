e96ecec102070450ba09b1435bbf1074b38667b6
Lucca Romaniello <37159767+luccaromaniello@users.noreply.github.com>
2026-09-11 15:18:58 -0300

    ci: pattern updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://www.luccaromaniello.com",
+  site: "https://lucca.fyi",
   integrations: [mdx(), sitemap()],
 
   vite: {
